641a30ec9067ce6ded56650d64f98e7b661b092f
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-12 03:38:27 -0400

    re configurando firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,18 @@
-import admin from 'firebase-admin';
-import { createRequire } from 'module';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import fs from 'fs';
+import { URL } from 'url';
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require('./firebase-keys.json');
+// se le dice donde esta la key de manera especifica, sin esto no me dejaba inicializar
+const jsonPath = new URL('./firebase-keys.json', import.meta.url);
+const serviceAccount = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+// se inicializa firebase usando la función directa 'cert' que exporta la v14 de Firebase
+initializeApp({
+  credential: cert(serviceAccount)
 });
 
-console.log('firebase inicializado correctamente');
+console.log('Firebase Admin inicializado correctamente en el Backend ✓');
 
-export const auth = admin.auth();
+// se exportamos el módulo auth llamando directamente a getAuth()
+export const auth = getAuth();
