5bd506699b885a860629c051212310767a934edf
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-12 03:20:52 -0400

    configuracion de rutas hecha imports añadidos a index y aplicamos firebase-keys.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import './config/firebase.js'; // para echar a andar firebase
+import usuariosRoutes from './routes/usuarios.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
@@ -13,6 +15,9 @@ app.use(express.json()); // para leer lo de front
 app.get('/', (req, res)=>{
     res.json({ message: 'API GESTIUSUARIOS ARRIBA'});
 });
+
+// enlazamos rutas con:
+app.use('/api/usuarios', usuariosRoutes);
 
 app.listen(PORT, () => {
     console.log(`usuarios corriendo en puerto ${PORT}`);
