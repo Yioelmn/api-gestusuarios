9bac5f86cb7004a6b796701b5c131f35decdbdf8
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-12 00:34:36 -0400

    poblacion de tabla usuarios en neon

--- src/config/db.js
+++ src/config/db.js
@@ -12,6 +12,19 @@ export const pool = new Pool({
 
 const initDB = async () => {
     try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id SERIAL PRIMARY KEY,
+                nombre VARCHAR(100) NOT NULL,
+                apellidos VARCHAR(100) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                fecha_nacimiento DATE NOT NULL,
+                direccion VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                rol VARCHAR(50) DEFAULT 'user',
+                creado_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
 
     } catch (e){
         console.error('Error al inicializar la base de datos', e)
