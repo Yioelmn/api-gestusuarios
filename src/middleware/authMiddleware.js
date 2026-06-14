e6ccfd5e6cde60c50334f1d8a86c55cd856711bb
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-14 09:00:43 -0400

    re adaptando middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,20 +1,22 @@
 import { auth } from '../config/firebase.js';
 
-export const verificarToken = async (requestAnimationFrame, resizeBy, next) => {
+export const verificarToken = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        return res.status(401).json({ error: 'Acceso denegado, token invalido'});
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Acceso denegado, token inválido o ausente' });
+    }
 
-        const token = authHeader.split(' ')[1];
+    const token = authHeader.split(' ')[1];
 
-        try{
-            const decodedToken = await auth.verifyIdToken(token);
-            req.usuario = decodedToken; //guarda los datos del usuario logueado
-            next();
-        }catch(e){
-            console.error('Error al verificar token:', e);
-            return res.status(403).json({ error: 'Token invalido'});
-        }
-    };
-}
+    try {
+        const decodedToken = await auth.verifyIdToken(token);
+        
+        req.usuario = decodedToken; 
+        
+        next(); 
+    } catch (e) {
+        console.error('Error al verificar token:', e);
+        return res.status(403).json({ error: 'Token inválido o expirado' });
+    }
+};
